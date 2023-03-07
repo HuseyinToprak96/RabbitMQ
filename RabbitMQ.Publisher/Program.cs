d1e8dd649fb77c59eba1424e956448e04366fe74
Huseyin Toprak <101343622+HuseyinToprak96@users.noreply.github.com>
2023-03-07 19:57:55 +0300

    Add project files.

---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMQ.Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://oaoefwoc:GakX6_Y-JQyumxc5fU9z998UJNZ5lHLO@rattlesnake.rmq.cloudamqp.com/oaoefwoc");//RabbitMQ Manager içinde CloudAMQP URL i
+
+            using var connection = factory.CreateConnection();//bağlantı açılır.
+
+            var channel = connection.CreateModel();//kanal oluşturulur.
+
+            channel.QueueDeclare("hello-queue", true, false, false);//Kuyruk oluşturulur.
+                                                                    //1. rabbitMQ ismi 2. Kuyruk fiziksel  olarak kaydedilsinmi? 3. true olur ise sadece bu kanaldan ulaşılabilsin. false ise farklı yerlerden erişilebilsin.
+                                                                    //4. kuyruk down olunca silinsin mi? 5. 
+
+
+            string message = "Hello world";
+            //RabbitMQ ya mesajlar byte dizisi olarak gönderilir.
+            var messageBody = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+
+            Console.WriteLine("Mesaj gönderilmiştir.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RabbitMQ.Subscriber/Program.cs
@@ -0,0 +1,36 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace RabbitMQ.Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://oaoefwoc:GakX6_Y-JQyumxc5fU9z998UJNZ5lHLO@rattlesnake.rmq.cloudamqp.com/oaoefwoc");//RabbitMQ Manager içinde CloudAMQP URL i
+
+            using var connection = factory.CreateConnection();//bağlantı açılır.
+
+            var channel = connection.CreateModel();//kanal oluşturulur.
+
+            channel.QueueDeclare("hello-queue", true, false, false);//Kuyruk oluşturulur.
+            //1. rabbitMQ ismi 2. Kuyruk fiziksel  olarak kaydedilsinmi? 3. true olur ise sadece bu kanaldan ulaşılabilsin. false ise farklı yerlerden erişilebilsin.
+            var consumer = new EventingBasicConsumer(channel);
+
+            channel.BasicConsume("hello-queue", true, consumer);
+            //1. bu kanal hangi kuyruğu izliycek
+            //2. true olursa kuyruktan 1 mesaj gönderildiğinde doğruda işlense yanlışta işlense siler, false ise kuyruktan silme ben sana silceğin zamanı söyliycem anlamındadır.
+            //normalde 2. false olur çünkü yanlış gönderilebilir.
+            consumer.Received += (object sender, BasicDeliverEventArgs e)=> 
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+                Console.WriteLine("Gelen Mesaj:" + message);
+                Console.ReadLine();
+            };
+        }
+    }
+}
